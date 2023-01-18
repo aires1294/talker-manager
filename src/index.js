7799c504f215426a76386d84639557d2e18eccfb
aires1294 <pedrotja.engenharia@gmail.com>
2023-01-18 10:52:08 -0300

    item 1 feitoo

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
-// const express = require('express');
-// const { readTalkerData } = require('./utils/fsUtils');
-
-// const app = express();
-// app.use(express.json());
-
-// const HTTP_OK_STATUS = 200;
-// const PORT = '3010';
-
-// // não remova esse endpoint, e para o avaliador funcionar
-// app.get('/', (_request, response) => {
-//   response.status(HTTP_OK_STATUS).send();
-// });
-
-// app.get('/talker', async (request, response) => {
-//   const talkers = await readTalkerData();
-//   return response.status(HTTP_OK_STATUS).json({ talkers });
-// });
-
-// app.listen(PORT, () => {
-//   console.log('Online');
-// });
-
 const express = require('express');
-// const { readTalkerData } = require('./utils/fsUtils');
-const talkers = require('./talker.json');
+const { readTalkerData } = require('./utils/fsUtils');
 
 const app = express();
 app.use(express.json());
@@ -36,7 +12,11 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (request, response) => {
+app.get('/talker', async (_request, response) => {
+  const talkers = await readTalkerData();
+  if (!talkers) {
+    return response.status(HTTP_OK_STATUS).json([]);
+  } 
   return response.status(HTTP_OK_STATUS).json(talkers);
 });
 
--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -1,40 +1,19 @@
-// const fs = require('fs').promises;
-// const path = require('path');
+const fs = require('fs').promises;
+const path = require('path');
 
-// const pathName = 'talker.json';
+const DataPathName = '../talker.json';
 
-// async function readTalkerData() {
-//     try {
-//         const data = await fs.readFile(path.resolve(__dirname, pathName));
-//         const talker = JSON.parse(data);
-//         // console.log(talker);
-//         return talker;
-//     } catch (er) {
-//         return console.log(`Erro na leitura do arquivo ${er.message}`);
-//     }
-// }
+async function readTalkerData() {
+    try {
+        const data = await fs.readFile(path.resolve(__dirname, DataPathName));
+        const talker = JSON.parse(data);
+        // console.log(talker);
+        return talker;
+    } catch (er) {
+        return console.log(`Erro na leitura do arquivo ${er.message}`);
+    }
+}
 
-// module.exports = {
-//     readTalkerData,
-// };
-
-// const fs = require('fs').promises;
-// // const path = require('path');
-// // const talker = require('../talker.json');
-
-// // const pathName = 'talker.json';
-
-// async function readTalkerData() {
-//     try {
-//         const data = await fs.readFile('../talker.json');
-//         const dataTalkerJson = JSON.parse(data);
-//         return dataTalkerJson;
-//         // console.log(talker);
-//     } catch (er) {
-//         return console.log(`Erro na leitura do arquivo ${er.message}`);
-//     }
-// }
-
-// module.exports = {
-//     readTalkerData,
-// };
+module.exports = {
+    readTalkerData,
+};
